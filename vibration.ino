53c4a2db5fc1573a9d815d7156b92d61bc7bdf9d
Roshith PB <103378908+pb-roshith@users.noreply.github.com>
2024-01-09 19:44:50 +0530

    Update vibration.ino

--- vibration.ino
+++ vibration.ino
@@ -1,14 +1,20 @@
-int led = 13;
-int vib = A0;
-
+int vib_pin=7;
+int led_pin=13;
 void setup() {
-  pinMode(led, OUTPUT);
-  pinMode(vib, INPUT);
-  Serial.begin(9600);
+  pinMode(vib_pin,INPUT);
+  pinMode(led_pin,OUTPUT);
 }
 
 void loop() {
-  long measure = pulseIn(vib, HIGH);
-  delay(100);
-  Serial.println(measure);
+  int val;
+  val=digitalRead(vib_pin);
+  if(val==1)
+  {
+    digitalWrite(led_pin,HIGH);
+    delay(1000);
+    digitalWrite(led_pin,LOW);
+    delay(1000);
+  }else{
+    digitalWrite(led_pin,LOW);
+  }
 }
